2ac42345032dc423e848e7f06fceda8563baf320
Daniel <dpengmarais99@gmail.com>
2025-01-18 12:36:31 +0200

    Completed the task tracker app. Preparing to deploy to json server

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import {useState} from 'react'
 import Header from '../src/components/Header';
 import Tasks from './components/Tasks';
+import AddTask from './components/AddTask';
 
 const App = () => {
+  const [showAddTask, setShowAddTask] = useState(true)
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -23,10 +25,18 @@ const App = () => {
       reminder: false,
     },
   ])
-  
+
+// Add Task
+const addTask =  (task) =>{
+  const id = Math.floor(Math.random() * 10000) + 1
+  const newTask = {id, ...task}
+  setTasks([...tasks, newTask])
+}
+
 // Delete Task 
 const deleteTask =(id) =>{
   setTasks(tasks.filter((task)=> task.id !== id))
+ 
 }
 
 // Toggle Reminder
@@ -37,7 +47,8 @@ setTasks(tasks.map((task) => task.id === id
 
   return (
     <div className="container">
-      <Header /> {/* No need to pass props; it will use defaultProps */}
+      <Header onAdd= {() => setShowAddTask(!showAddTask)} showAdd = {showAddTask}/>
+      {showAddTask && <AddTask onAdd= {addTask} /> }  
       {tasks.length > 0 ? <Tasks tasks= {tasks} onDelete = {deleteTask} onToggle = {toggleReminder} />: 'There is no more tasks left.'}
     </div>
   );
